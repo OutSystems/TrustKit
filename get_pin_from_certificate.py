b313bc729f0708f13b18b39aaabd46f618c34ff4
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2024-04-29 16:15:55 +0100

    chore: Address Snyk issues
    Remove Demo project.
    Replace `get_pin_from_certificate.py` file for the one available on the `cordova-outsystems-sslpinning` branch.

--- get_pin_from_certificate.py
+++ get_pin_from_certificate.py
@@ -1,51 +1,43 @@
-#!/usr/bin/env python
-"""Helper script to generate a TrustKit or HPKP pin from a PEM/DER certificate file.
+#!/usr/bin/env python2.7
 """
-from __future__ import print_function
-from subprocess import Popen, PIPE
-from sys import stdin
+Original source: https://github.com/datatheorem/TrustKit/blob/master/get_pin_from_certificate.py
+Helper script to generate a TrustKit or HPKP pin from a PEM/DER certificate file.
+"""
+from subprocess import check_output
 import os.path
 import argparse
 import hashlib
 import base64
-import platform
-import io
 
 
 class SupportedKeyAlgorithmsEnum(object):
     RSA_2048 = 1
     RSA_4096 = 2
     ECDSA_SECP256R1 = 3
-    ECDSA_SECP384R1 = 4
 
 
 if __name__ == '__main__':
     # Parse the command line
     parser = argparse.ArgumentParser(description='Generate HPKP / TrustKit SSL Pins.')
-    parser.add_argument('certificate', metavar='FILE', nargs='?', help='certificate file to read, if empty, stdin is used')
+    parser.add_argument('certificate')
     parser.add_argument('--type', dest='type', action='store', default='PEM',
                         help='Certificate file type; "PEM" (default) or "DER".')
     args = parser.parse_args()
 
-    if not args.certificate:
-        certificate = stdin.read()
-    elif os.path.isfile(args.certificate):
-        with io.open(args.certificate, 'rb') as certFile:
-            certificate = certFile.read()
-    else:
+    if not os.path.isfile(args.certificate):
         raise ValueError('Could not open certificate file {}'.format(args.certificate))
 
     if args.type not in ['DER', 'PEM']:
         raise ValueError('Invalid certificate type {}; expected DER or PEM'.format(args.type))
 
-    # Parse the certificate and print its information
-    p1 = Popen('openssl x509 -inform {} -text -noout'.format(args.type), shell=True, stdin=PIPE, stdout=PIPE)
-    certificate_txt = p1.communicate(input=certificate)[0].decode('utf-8')
 
-    print('\nCERTIFICATE INFO\n----------------')
-    p1 = Popen('openssl x509 -subject -issuer -fingerprint -sha1 -noout -inform {}'.format(
-        args.type), shell=True, stdin=PIPE, stdout=PIPE)
-    print(p1.communicate(input=certificate)[0])
+    # Parse the certificate and print its information
+    certificate_txt = check_output('openssl x509 -inform {} -in {} -text -noout'.format(args.type,
+                                                                                        args.certificate), shell=True)
+    print '\nCERTIFICATE INFO\n----------------'
+    print check_output('openssl x509 -subject -issuer -fingerprint -sha1 -noout -inform {} -in {}'.format(
+        args.type, args.certificate), shell=True)
+
 
     # Extract the certificate key's algorithm
     # Tested on the output of OpenSSL 0.9.8zh and OpenSSL 1.0.2i
@@ -54,8 +46,6 @@ if __name__ == '__main__':
     if alg_txt == 'id-ecPublicKey':
         if 'prime256v1' in certificate_txt:
             key_algorithm = SupportedKeyAlgorithmsEnum.ECDSA_SECP256R1
-        if 'secp384r1' in certificate_txt:
-            key_algorithm = SupportedKeyAlgorithmsEnum.ECDSA_SECP384R1
     elif alg_txt == 'rsaEncryption':
         if 'Key: (2048 bit)' in certificate_txt:
             key_algorithm = SupportedKeyAlgorithmsEnum.RSA_2048
@@ -63,35 +53,33 @@ if __name__ == '__main__':
             key_algorithm = SupportedKeyAlgorithmsEnum.RSA_4096
 
     if key_algorithm is None:
-        raise ValueError('Error: Certificate key algorithm not supported: {}.'.format(alg_txt))
+        raise ValueError('Error: Certificate key algorithm not supported.')
 
 
     # Generate the Subject Public Key Info hash
     if key_algorithm == SupportedKeyAlgorithmsEnum.ECDSA_SECP256R1:
-        # The OpenSSL command is different for ECDSA secp256
+        # The OpenSSL command is different for ECDSA
         openssl_alg = 'ec'
-    elif key_algorithm == SupportedKeyAlgorithmsEnum.ECDSA_SECP384R1:
-        # The OpenSSL command is different for ECDSA secp384
-        openssl_alg = 'ec'
+        trustkit_alg = 'kTSKAlgorithmEcDsaSecp256r1'
     elif key_algorithm == SupportedKeyAlgorithmsEnum.RSA_2048:
         openssl_alg = 'rsa'
+        trustkit_alg = 'kTSKAlgorithmRsa2048'
     elif key_algorithm == SupportedKeyAlgorithmsEnum.RSA_4096:
         openssl_alg = 'rsa'
+        trustkit_alg = 'kTSKAlgorithmRsa4096'
     else:
         raise ValueError('Unexpected key algorithm')
 
-    if platform.system() == 'Windows':
-        cmd_redirects = '2>nul'
-    else:
-        cmd_redirects = '-in /dev/stdin 2>/dev/null'
-        
-    p1 = Popen('openssl x509  -pubkey -noout -inform {} '
-               '| openssl {} -outform DER -pubin {}'.format(args.type, openssl_alg, cmd_redirects),
-            shell=True, stdin=PIPE, stdout=PIPE)
-    spki = p1.communicate(input=certificate)[0]
+    spki = check_output('openssl x509  -pubkey -noout -inform {} -in {} '
+                        '| openssl {} -outform DER -pubin -in /dev/stdin 2>/dev/null'.format(args.type,
+                                                                                             args.certificate,
+                                                                                             openssl_alg),
+                        shell=True)
 
     spki_hash = hashlib.sha256(spki).digest()
     hpkp_pin = base64.b64encode(spki_hash)
 
-    print('\nTRUSTKIT CONFIGURATION\n----------------------')
-    print('kTSKPublicKeyHashes: @[@"{}"] // You will also need to configure a backup pin'.format(hpkp_pin))
+    print 'TRUSTKIT CONFIGURATION\n----------------------'
+    print 'kTSKPublicKeyHashes: @[@"{}"] // You will also need to configure a backup pin'.format(hpkp_pin)
+    print 'kTSKPublicKeyAlgorithms: @[{}]\n'.format(trustkit_alg)
+    
